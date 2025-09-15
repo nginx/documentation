ddeeefc808452bfbcbe48de738ccbd096b61bc1a
Maximus Smith <maximusmith529@gmail.com>
2025-09-15 16:21:34 -0700

    Testing: N4A Button testing

--- tests/n4a-calculator.spec.ts
+++ tests/n4a-calculator.spec.ts
@@ -2,8 +2,30 @@ import { expect, test } from '@playwright/test';
 // import { handleConsentPopup, waitFor } from './util';
 
 // Unit test for the accuracy of the calculations
+test.describe('Numerical test for N4A calculator page', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/test-product');
+        await page.waitForLoadState('load');
+    });
+
+    test('estimageNCUUsage calculations are accurate', async ({page}) => {
+        await page.getByTestId("button-ncu-usage-details").click();
+        await page.getByTestId("button-total-cost-details").click();
+    });
+});
 
 // UI test to click buttons to test functionality
+test.describe('Button functionality test for N4A calculator page', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/test-product');
+        await page.waitForLoadState('load');
+    });
+
+    test('estimateNCUUsage buttons are functional', async ({page}) => {
+        await page.getByTestId("button-ncu-usage-details").click();
+        await page.getByTestId("button-total-cost-details").click();
+    });
+});
 
 // UI test to make sure the page renders correctly. IE. Calculator with #s on it, regions, etc are populated
 test.describe('Smoke test for calculation page', () => {
