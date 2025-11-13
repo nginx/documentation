f6c739da23b80f3f226f0e4db75fb95bf51fb281
Naveen GOPU <n.gopu@f5.com>
2025-11-13 16:44:40 +0530

    NLB-7074: Revert js fix

--- static/nginxaas-azure/js/cost-calculator_v2.js
+++ static/nginxaas-azure/js/cost-calculator_v2.js
@@ -367,10 +367,7 @@
       if (curEl.tagName.toLowerCase() === "input" || curEl.tagName.toLowerCase() === "select") {
         curEl.value = values[elName];
       } else {
-        const inputEl = curEl.querySelector("input");
-        if (inputEl) {
-          inputEl.value = values[elName];
-        }
+        $(curEl).children("input").first().value = values[elName];
       }
     });
   };
@@ -386,10 +383,7 @@
       if (curEl.tagName.toLowerCase() === "input" || curEl.tagName.toLowerCase() === "select") {
         curEl.value = values[elName];
       } else {
-        const inputEl = curEl.querySelector("input");
-        if (inputEl) {
-          inputEl.value = values[elName];
-        }
+        $(curEl).children("input").first().value = values[elName];
       }
     });
 
