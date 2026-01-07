10966b5bc68cb93deafcfd5967ae28da22e2b27a
Lam Nguyen <la.nguyen@f5.com>
2026-01-07 13:12:45 -0800

    fix: Run the playwright action locally instead of prod

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig, devices } from '@playwright/test';
+
+const BASE_URL = 'http://127.0.0.1';
+const PORT = 1313;
+const BASE_HUGO_CMD_PATH = '/github/home/actions_hugo/bin/hugo';
+
 export default defineConfig({
     testDir: './src',
     fullyParallel: true,
     workers: 1,
     outputDir: './test-results',
-    reporter: [['html', {  outputFolder: './playwright-report' }]],
+    reporter: [['html', { open: 'never', outputFolder: './playwright-report' }]],
     projects: [
         {
             name: 'chromium',
@@ -25,10 +30,16 @@ export default defineConfig({
     ],
     use: {
         // Base URL to use in actions like `await page.goto('/')`.
-        baseURL: 'https://docs.nginx.com',
+        baseURL: `${BASE_URL}:${PORT}`,
+        trace: 'on-first-retry',
         // Set Geolocation to Cork, Ireland
         geolocation: { longitude: -8.486316, latitude: 51.896893 },
         permissions: ['geolocation'],
         video: 'retain-on-failure'
       },
+    webServer: {
+        command: `cd ../ && ${BASE_HUGO_CMD_PATH} mod get && ${BASE_HUGO_CMD_PATH} serve --port ${PORT}`,
+        url: `${BASE_URL}:${PORT}`,
+        stdout: 'ignore',
+  },
 })
